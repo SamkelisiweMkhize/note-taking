ad3afd5981469b86f8a964ce6780d40699d457f3
Anella2 <dimaniyonela4@gmail.com>
2024-05-27 11:02:55 +0200

    form-submit-html

--- note-taking/src/app/app.module.ts
+++ note-taking/src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NatesPageComponent } from './pages/nates-page/nates-page.component';
 import { NotesPageComponent } from './pages/notes-page/notes-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NatesPageComponent,
+  
     NotesPageComponent
   ],
   imports: [
